652d22f64c252ddb878b4723c2e11b3a5ff31ce9
masonmil <masonmil@umich.edu>
2023-09-10 22:41:54 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+import numpy as np
+
+pygame.init()
+font = pygame.font.SysFont('arial', 25)
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+Point = namedtuple('Point', 'x, y')
